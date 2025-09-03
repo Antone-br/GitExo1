d3f3c5cdab8d4c55ce0652d9379c804b5c058d77
Antone BARBAUD <abarb@lmdsio.com>
2025-09-03 15:00:33 +0200

    formulaire prêts

---
+++ accueil.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Accueil</title>
+</head>
+<body>
+<h1>Accueil</h1>
+
+<?php
+if (isset($_POST['prenom']) && isset($_POST['nom'])) {
+    $prenom = htmlspecialchars($_POST['prenom']);
+    $nom = htmlspecialchars($_POST['nom']);
+    echo "<p>Bienvenue, $prenom $nom !</p>";
+} else {
+    echo "<p>Informations manquantes.</p>";
+}
+?>
+
+</body>
+</html>
